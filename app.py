71aa4f2fa14bbf36c46af0bf4c63f96cb5613e35
da.pilipenko <da.pilipenko@istu.edu.ua>
2025-05-13 19:30:59 +0300

    add authentication for user

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 from db import db
 from appmodels import *
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -18,6 +21,42 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
+    @jwt.additional_claims_loader
+    
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+    
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+        
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+        
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+        
     db.init_app(app)
     api = Api(app)
     with app.app_context():
@@ -25,4 +64,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
     return app
+
--- appmodels/__init__.py
+++ appmodels/__init__.py
@@ -2,3 +2,4 @@ from appmodels.itemModel import ItemModel
 from appmodels.tagModel import TagModel
 from appmodels.storeModel import StoreModel
 from appmodels.item_tags import ItemsTags
+from appmodels.userModel import UserModel
